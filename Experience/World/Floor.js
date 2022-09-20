8a821646883de2069e9137a5e66f510892e09ba6
Taryn <95415824+tarynd9@users.noreply.github.com>
2022-09-20 14:03:29 +0100

    Update Floor.js

--- Experience/World/Floor.js
+++ Experience/World/Floor.js
@@ -25,9 +25,9 @@ export default class Floor {
 
     setCircles() {
         const geometry = new THREE.CircleGeometry(5, 64);
-        const material = new THREE.MeshStandardMaterial({ color: 0xe5a1aa });
-        const material2 = new THREE.MeshStandardMaterial({ color: 0x8395cd });
-        const material3 = new THREE.MeshStandardMaterial({ color: 0x7ad0ac });
+        const material = new THREE.MeshStandardMaterial({ color: 0x0076CE });
+        const material2 = new THREE.MeshStandardMaterial({ color: 0x00447C });
+        const material3 = new THREE.MeshStandardMaterial({ color: 0x41B6E6 });
 
         this.circleFirst = new THREE.Mesh(geometry, material);
         this.circleSecond = new THREE.Mesh(geometry, material2);
